35091b3e9ba6f73f2f8727cb069f2315e4ab5bf8
Marviin1999 <161788396+Marviin1999@users.noreply.github.com>
2026-05-11 01:05:21 -0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hr8)pwdtv3uy_tt0#o4f#8i%*#^m*%cn=zen=v^-2#er-qx6al
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', '.onrender.com']
+ALLOWED_HOSTS = ['blow-hyu7.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
